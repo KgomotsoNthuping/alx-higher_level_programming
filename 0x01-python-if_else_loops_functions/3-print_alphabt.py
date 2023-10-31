5b16a9e486a1e391661ddd024569f5c7020042eb
Kgomotso222 <138695830+Kgomotso222@users.noreply.github.com>
2023-10-31 21:18:21 +0200

    python

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 for alpha_letters in range(ord('a'), ord('z') + 1):
-    if alpha_letters in [101,113]:
-       continue
-    print("{:c}".format(alpha_letters), end="")
+    if chr(alpha_letters) != 'q' and chr(alpha_letters) != 'e':
+    print("{}".format(chr(alpha_letters)), end='')
