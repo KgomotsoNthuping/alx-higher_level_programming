fd2627bf2c45ec1d1988ed4baf2475922c43c254
Kgomotso222 <kgomotsonthuping22@gmail.com>
2023-10-31 19:07:29 +0200

    python

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-for num in range(0, 100):
-    print(f"{num`:02d}", end=', ' if num < 99 else '\n')
+for num in range(00, 99):
+    print("{:02d}".format(num), end=', ')
+    print("{:02d}".format(num + 1))
