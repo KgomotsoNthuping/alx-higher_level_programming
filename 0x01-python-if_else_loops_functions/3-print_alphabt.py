6027e0eb89192e944ca3f068ebd3d0574a4d9a51
Kgomotso222 <138695830+Kgomotso222@users.noreply.github.com>
2023-10-31 21:25:06 +0200

    python

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for alpha_letters in range(ord('a'), ord('z') + 1):
-    if chr(alpha_letters) != 'q' and chr(alpha_letters) != 'e':
-    print("{}".format(chr(alpha_letters)), end='')
+for m in range(97, 123):
+    if (m != 101 and m != 113):
+        print("{}".format(chr(m)), end='')
