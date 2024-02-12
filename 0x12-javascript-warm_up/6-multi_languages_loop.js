c361ee6909a026d2aba373991da84d5cd8acdcfb
Kgomotso Nthuping <138695830+KgomotsoNthuping@users.noreply.github.com>
2024-02-12 20:56:04 +0200

    javascript

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const arrLength = lines.length;
+let a = 0;
+while (a < arrLength) {
+  console.log(lines[a]);
+  a++;
+}
