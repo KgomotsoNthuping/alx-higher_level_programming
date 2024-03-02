71aea79760f5904842e85fa27d1fd2e0b6c2544e
Kgomotso Nthuping <138695830+KgomotsoNthuping@users.noreply.github.com>
2024-03-02 12:59:03 +0200

    pyt

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""A script that:
-- takes your GitHub credentials (username and password)
-- uses the GitHub API to display your id
-"""
-import sys
+"""Request from GitHub API"""
+
+from sys import argv
 import requests
-from requests.auth import HTTPBasicAuth
 
+if __name__ == '__main__':
+    url = 'https://api.github.com/user'
+    user = argv[1]
+    token = argv[2]
+    headers = {'Authorization': 'token {}'.format(token)}
 
-if __name__ == "__main__":
-    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
-    r = requests.get("https://api.github.com/user", auth=auth)
-    print(r.json().get("id"))
+    u = requests.get(url, headers=headers)
+    print(u.json().get('id'))
