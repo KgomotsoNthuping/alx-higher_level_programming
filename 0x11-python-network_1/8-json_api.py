b11a5d568294977984fb92b192c0aeb4399184dc
Kgomotso Nthuping <138695830+KgomotsoNthuping@users.noreply.github.com>
2024-03-02 13:23:09 +0200

    pyt

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,27 +1,18 @@
 #!/usr/bin/python3
-"""It takes in a letter and sends a POST request to the url
-"""
-
-from sys import argv
+"""Python script that takes in a letter and sends a POST request"""
 import requests
+from sys import argv
 
 if __name__ == '__main__':
+    q = argv[1] if len(argv) == 2 else ""
     url = 'http://0.0.0.0:5000/search_user'
-
-    if len(argv) > 1:
-        a = argv[1]
-    else:
-        a = ''
-
-    u = requests.post(url, data={'a': a})
-
+    r = requests.post(url, data={'q': q})
     try:
-        u = u.json()
-
-        if not u:
-            print('No result')
+        r_dict = r.json()
+        id, name = r_dict.get('id'), r_dict.get('name')
+        if len(r_dict) == 0 or not id or not name:
+            print("No result")
         else:
-            print('[{}] {}'.format(u.get('id'), u.get('name')))
-
-    except ValueError:
-        print('Not a valid JSON')
+            print("[{}] {}".format(r_dict.get('id'), r_dict.get('name')))
+    except Exception:
+        print("Not a valid JSON")
